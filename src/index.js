8662959df954a3fe25ee8af58752328754f8a588
AlinaKozz <alina.kozz58@gmail.com>
2019-03-03 13:26:53 +0300

    add a function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    const dict = {'H': 50, 'Q': 25, 'D': 10, 'N': 5, 'P': 1};
+    var result = {};
+    if (currency <= 0) {
+        return result;
+    } else if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    } else {
+        for (var value in dict) {
+            if (currency >= dict[value]) {
+                result[value] = Math.trunc(currency / dict[value]);
+                currency = currency % dict[value];
+            }
+        }
+        return result;
+    }
+};
